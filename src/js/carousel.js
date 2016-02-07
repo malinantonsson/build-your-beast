e0330a3a6f6b9a561eb33abe99aca3c84da2ecd1
Malin Antonsson <Malin.Antonsson@rga.com>
2016-02-07 11:56:29 +0000

    fix carousel for crown slide

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -5,6 +5,9 @@ $(document).ready(function(){
     maxSlides: 6,
     slideMargin: 0,
     moveSlides: 5,
-    adaptiveHeight: true
+    adaptiveHeight: true,
+    pager: false,
+    nextText: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>',
+    prevText: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>'
   });
 });
