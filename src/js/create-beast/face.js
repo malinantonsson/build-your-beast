5defcd0f2d05b1c9ac9fdc68c7f8280695638e77
Malin Antonsson <malin@malinantonsson.com>
2016-09-19 17:26:40 +0100

    Add intro animations

--- src/js/create-beast/face.js
+++ src/js/create-beast/face.js
@@ -1,16 +1,47 @@
 var svgData;
 
 var outerWrapper = document.querySelector('.byb-outside-wrapper');
+var introWrapper = document.querySelector('.intro-lightning');
 var introClass = 'is-intro';
+var hideIntroClass = 'intro-end';
+var introIsFinished = false;
+
+setTimeout(function() {
+	if(!introIsFinished) {
+		introIsFinished = true;
+	} 
+}, 3000);
+
+var finishIntro = function() {
+	introWrapper.classList.add(hideIntroClass);
+	setTimeout(function() {
+		outerWrapper.classList.remove(introClass);
+	}, 1000);
+};
 
 var request = new XMLHttpRequest();
-request.open('GET', '/data/beast-data.json', true);
 
+
+request.open('GET', '/data/beast-data.json', true);
+//setTimeout(function() {
+	request.send();
+//}, 2000);
 request.onload = function() {
   if (this.status >= 200 && this.status < 400) {
     // Success!
     svgData = JSON.parse(this.response);
-    outerWrapper.classList.remove(introClass);
+
+    if(introIsFinished) {
+	    finishIntro();
+	} else {
+		var checkIntro = setInterval(function() {
+			if(introIsFinished) {
+				clearInterval(checkIntro);
+				finishIntro();
+			}
+		}, 500);
+	}
+
   } else {
   	console.log(this.status);
   	//TODO: add error messages
@@ -26,7 +57,7 @@ request.onerror = function() {
   // There was a connection error of some sort
 };
 
-request.send();
+
 
 var sliders = $('body').find('.slider');
 
