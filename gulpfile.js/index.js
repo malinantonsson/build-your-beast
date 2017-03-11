d013964d823dadd1f38d24c951f2ee15e7448783
Malin Antonsson <malin@malinantonsson.com>
2017-03-11 17:43:14 +0000

    fix my messes

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,30 +1,107 @@
 'use strict';
 
-var config  = require('./config');
-var gulp    = require('gulp');
-var util    = require('gulp-util');
+var source = require('vinyl-source-stream');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var watchify = require('watchify');
+var notify = require('gulp-notify');
 
-// What mode?
-util.log('Running in', (config.production ? util.colors.red.bold('production') : util.colors.green.bold('development')), 'mode');
+var autoprefixer = require('gulp-autoprefixer');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var buffer = require('vinyl-buffer');
 
-// Load tasks
-var clean       = require('./tasks/clean');
-var eslint      = require('./tasks/eslint');
-var modernizr   = require('./tasks/modernizr');
-var scripts     = require('./tasks/scripts');
-var styles      = require('./tasks/styles');
-var pages       = require('./tasks/pages');
-var watch       = require('./tasks/watch');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var historyApiFallback = require('connect-history-api-fallback')
 
-// Define tasks and dependencies
-gulp.task('clean:scripts', clean.scripts);
-gulp.task('clean:styles', clean.styles);
-gulp.task('clean:pages', clean.pages);
-gulp.task('clean:all', clean.all);
-gulp.task('default', (config.production ? ['modernizr', 'scripts', 'styles'] : ['modernizr', 'scripts', 'styles', 'pages', 'watch']));
-gulp.task('eslint', eslint);
-gulp.task('modernizr', ['scripts', 'styles'], modernizr);
-gulp.task('scripts', ['clean:scripts', 'eslint'], scripts);
-gulp.task('styles', ['clean:styles'], styles);
-gulp.task('pages', ['clean:pages'], pages);
-gulp.task('watch', ['modernizr', 'scripts', 'styles'], watch);
+
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+
+gulp.task('styles', function () {
+ return gulp.src('./src/sass/**/*.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(autoprefixer())
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('./build/css/'))
+  .pipe(reload({stream:true}));
+});
+/*
+  Images
+*/
+gulp.task('images',function(){
+  gulp.src('css/images/**')
+    .pipe(gulp.dest('./build/css/images'))
+});
+
+/*
+  Browser Sync
+*/
+gulp.task('browser-sync', function() {
+    browserSync({
+        // we need to disable clicks and forms for when we test multiple rooms
+        server : {},
+        middleware : [ historyApiFallback() ],
+        ghostMode: false
+    });
+});
+
+function handleErrors() {
+  var args = Array.prototype.slice.call(arguments);
+  notify.onError({
+    title: 'Compile Error',
+    message: '<%= error.message %>'
+  }).apply(this, args);
+  this.emit('end'); // Keep gulp from hanging on this task
+}
+
+function buildScript(file, watch) {
+  var props = {
+    entries: ['src/scripts/' + file],
+    debug : true,
+    cache: {},
+    packageCache: {},
+    transform:  [babelify.configure({stage : 0 })]
+  };
+
+  // watchify() if watch requested, otherwise run browserify() once 
+  var bundler = watch ? watchify(browserify(props)) : browserify(props);
+
+  function rebundle() {
+    var stream = bundler.bundle();
+    return stream
+      .on('error', handleErrors)
+      .pipe(source(file))
+      .pipe(gulp.dest('./build/'))
+      // If you also want to uglify it
+      // .pipe(buffer())
+      // .pipe(uglify())
+      // .pipe(rename('app.min.js'))
+      // .pipe(gulp.dest('./build'))
+      .pipe(reload({stream:true}))
+  }
+
+  // listen for an update and run rebundle
+  bundler.on('update', function() {
+    rebundle();
+    gutil.log('Rebundle...');
+  });
+
+  // run it once the first time buildScript is called
+  return rebundle();
+}
+
+gulp.task('scripts', function() {
+  return buildScript('main.js', false); // this will run once because we set watch to false
+});
+
+// run 'scripts' task first, then watch for future changes
+gulp.task('default', ['images','styles','scripts','browser-sync'], function() {
+  gulp.watch('src/sass/**/*', ['styles']); // gulp watch for sass changes
+  return buildScript('main.js', true); // browserify watch for JS changes
+});
