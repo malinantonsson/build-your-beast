9374dd97a3b7ce97b8c53de466c01b77de4fc3e2
Malin Antonsson <malin@malinantonsson.com>
2017-10-21 22:51:17 +0100

    change into slick carousel

--- src/js/create-beast/carousel.js
+++ src/js/create-beast/carousel.js
@@ -1,12 +1,27 @@
 var initCarousel = function(carousel) {
-    $(carousel).bxSlider({
-        slideWidth: 180,
-        minSlides: 2,
-        maxSlides: 6,
-        slideMargin: 0,
-        adaptiveHeight: true,
-        pager: false,
-        nextText: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>',
-        prevText: '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>'
+    $(carousel).slick({
+
+        slidesToScroll: 2,
+        slidesToShow: 2,
+        slide: '.slide',
+        mobileFirst: true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 5,
+              slidesToScroll: 5
+            }
+          },
+          {
+            breakpoint: 767,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 4
+            }
+          },
+        ],
+        nextArrow: '<div class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg></div>',
+        prevArrow: '<div class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg></div>'
     });
 };
